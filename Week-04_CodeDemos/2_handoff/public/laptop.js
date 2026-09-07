76f0b6058948d85407dd39c10e0324577a2fcc93
lumiestorey <emil0021@e.ntu.edu.sg>
2026-09-07 16:02:55 +0800

    finalising changes

--- Week-04_CodeDemos/2_handoff/public/laptop.js
+++ Week-04_CodeDemos/2_handoff/public/laptop.js
@@ -1,11 +1,18 @@
 let socket;
 let input = { dx: 0, dy: 0 };
-let player = { x: 50, y: 0, vy: 0, w: 30, h: 30 };
+let player = { x: 50, y: 0, vy: 0, w: 30, h: 30, onGround: false};
 let gravity = 0.6;
 let ground = 350;
 let speed = 4;
-let platform = { x: 300, y: 220, w: 140, h: 16 };
+let platforms = [ 
+  { x: 100, y: 300, w: 50, h: 16 }, 
+  { x: 200, y: 200, w: 100, h: 16},
+  { x: 400, y: 300, w: 140, h: 16 }
+];
 let connected = false;
+
+let deepColor = "#f7eb9e";
+let highColor = "#a4f5fc";
 
 function setup() {
   createCanvas(600, 400);
@@ -25,7 +32,9 @@ function setup() {
 }
 
 function draw() {
-  background(20);
+  let t = constrain(map(player.y, 0, ground, 0, 1), 0, 1);
+  let bg = lerpColor(color(deepColor), color(highColor),t);
+  background(bg);
 
   player.x += input.dx * speed;
   player.x = constrain(player.x, 0, width - player.w);
@@ -33,27 +42,47 @@ function draw() {
   player.vy += gravity;
   player.y += player.vy;
 
+  player.onGround = false;
+
   if (player.y + player.h > ground) {
     player.y = ground - player.h;
     player.vy = 0;
+    player.onGround = true;
   }
 
-  let landing =
-    player.x + player.w > platform.x &&
-    player.x < platform.x + platform.w &&
-    player.y + player.h > platform.y &&
-    player.y + player.h < platform.y + platform.h + 10 &&
+  for (let i = platforms.length - 1; i >= 0; i--) {
+    let landingOnPlatform = 
+    player.x + player.w > platforms[i].x &&
+    player.x < platforms[i].x + platforms[i].w &&
+    player.y + player.h > platforms[i].y &&
+    player.y < platforms[i].y + platforms[i].h + 10 &&
     player.vy >= 0;
 
-  if (landing) {
-    player.y = platform.y - player.h;
+  if (landingOnPlatform) {
+    player.y = platforms[i].y - player.h;
     player.vy = 0;
+    player.onGround = true;
+    }
   }
 
+  // let landing =
+  //   player.x + player.w > platform.x &&
+  //   player.x < platform.x + platform.w &&
+  //   player.y + player.h > platform.y &&
+  //   player.y + player.h < platform.y + platform.h + 10 &&
+  //   player.vy >= 0;
+
+  // if (landing) {
+  //   player.y = platform.y - player.h;
+  //   player.vy = 0;
+  // }
+  noStroke();
   fill(60);
   rect(0, ground, width, height - ground);
   fill(90);
-  rect(platform.x, platform.y, platform.w, platform.h);
+  for (let i = 0; i < platforms.length; i++) {
+    rect(platforms[i].x, platforms[i].y, platforms[i].w, platforms[i].h);
+  }
 
   fill(240, 90, 60);
   rect(player.x, player.y, player.w, player.h);
@@ -64,7 +93,7 @@ function draw() {
 }
 
 function keyPressed() {
-  if (key === ' ') doJump();
+  if (key === ' ' && player.onGround) doJump();
 }
 
 function doJump() {
